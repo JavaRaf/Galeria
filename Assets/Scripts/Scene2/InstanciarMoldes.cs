e036e5983549c1618bbed253ec2c11d772bcc568
JavaRaf <rafaelconceicao722@gmail.com>
2024-08-06 17:13:08 -0300

    intanciação em tempo real

--- Assets/Scripts/Scene2/InstanciarMoldes.cs
+++ Assets/Scripts/Scene2/InstanciarMoldes.cs
@@ -4,16 +4,16 @@ using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using TMPro;
 
 public class LoadImage : MonoBehaviour
 {
     public GameObject prefab;    
     public GameObject panel;    
-    public string serverImagesUrl = "http://localhost:3000/images";
-
+    public string serverUrl = "http://localhost:3000/images";
     public string imagesEndPoint = "http://localhost:3000/uploads";
 
-    private List<Texture> images = new List<Texture>();
+    private ImageDataList dataList;
 
     [Serializable]
     public class ImageData
@@ -33,37 +33,37 @@ public class LoadImage : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(GetTextures(serverImagesUrl));
+        StartCoroutine(GetConnectionString());
     }
 
-    IEnumerator GetTextures(string url)
+    IEnumerator GetConnectionString()
     {
-        UnityWebRequest request = UnityWebRequest.Get(url);
+        UnityWebRequest request = UnityWebRequest.Get(serverUrl);
         yield return request.SendWebRequest();
-
-        Debug.Log("request: " + request);
 
         if (request.result == UnityWebRequest.Result.Success)
         {
             string jsonString = request.downloadHandler.text;
-
-            ImageDataList imageDataList = JsonUtility.FromJson<ImageDataList>(jsonString);
-
-            foreach (ImageData imageData in imageDataList.images)
-            {
-                string imageUrl = $"{imagesEndPoint}/{imageData.filename}"; 
-                yield return StartCoroutine(DownloadImage(imageUrl));
-            }
-
-            InstanciarObject();
+            dataList = JsonUtility.FromJson<ImageDataList>(jsonString);
+            // Chama a função passUrl após o download dos dados
+            StartCoroutine(PassUrl());
         }
         else
         {
-            Debug.LogError("Erro ao obter as URLs das imagens: " + request.error);
+            Debug.LogError("Error: " + request.error);
         }
     }
 
-    IEnumerator DownloadImage(string imageUrl)
+    IEnumerator PassUrl()
+    {
+        foreach (var data in dataList.images)
+        {
+            string imageUrl = $"{imagesEndPoint}/{data.filename}";
+            yield return StartCoroutine(DownloadImage(imageUrl, data.filename));
+        }
+    }
+
+    IEnumerator DownloadImage(string imageUrl, string filename)
     {
         UnityWebRequest request = UnityWebRequestTexture.GetTexture(imageUrl);
         yield return request.SendWebRequest();
@@ -71,27 +71,20 @@ public class LoadImage : MonoBehaviour
         if (request.result == UnityWebRequest.Result.Success)
         {
             Texture texture = DownloadHandlerTexture.GetContent(request);
-            images.Add(texture);
+            // Instanciar o prefab
+            GameObject newObject = Instantiate(prefab, panel.transform);
+            // Definir a textura no componente RawImage do prefab
+            RawImage rawImageComponent = newObject.GetComponent<RawImage>();
+            TextMeshProUGUI filenameText = newObject.GetComponentInChildren<TextMeshProUGUI>();
+            if (rawImageComponent != null && filenameText != null)
+            {
+                rawImageComponent.texture = texture;
+                filenameText.text = filename;
+            }
         }
         else
         {
-            Debug.LogError("Erro ao baixar imagem: " + request.error);
-        }
-    }
-
-    void InstanciarObject()
-    {
-        foreach (Texture texture in images)
-        {
-            GameObject instancia = Instantiate(prefab, panel.transform);
-            if (instancia != null)
-            {
-                RawImage rawImage = instancia.GetComponent<RawImage>();
-                if (rawImage != null)
-                {
-                    rawImage.texture = texture;
-                }
-            }
+            Debug.LogError("Error: " + request.error);
         }
     }
 }
